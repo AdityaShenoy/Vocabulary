74c1d0aed18f44488018a9977a409a289ac04ac3
AdityaShenoy <adityashenoy49@gmail.com>
2020-04-12 11:49:56 +0530

    .

--- version3/main.py
+++ version3/main.py
@@ -1,31 +1,28 @@
-import pyautogui as pag
-import time
 from eng_to_hin import eng2hin
+import pyperclip
 
 # Initialize word count
-word_count = 1
+word_count = input()
 
-# Open the input file
-with open('input.txt') as f:
+# Initialize empty result
+result = []
 
-  # Iterate through all lines in the 
-  lines = list(map(str.strip, f.readlines()))
+# Iterate through all lines in the 
+lines = list(map(str.strip, [input() for _ in range(6)]))
 
-  for i in range(0, len(lines), 7):
-    
-    # Print output
-    print(f'*{word_count}. {lines[i]}*')
+# Append output to result
+result.append(f'*{word_count}. {lines[0]}*')
 
-    # Increment the word count
-    word_count += 1
+# Convert english pronounciation to hindi
+lines[1] = eng2hin(lines[1])
 
-    # Convert english pronounciation to hindi
-    lines[i + 1] = eng2hin(lines[i + 1])
+# These are the titles in the vocabulary card
+titles = ['Pronounciation', 'Definition', 'Self Definition',
+          'Mnemonic', 'Example Sentence']
 
-    # These are the titles in the vocabulary card
-    titles = ['Pronounciation', 'Definition', 'Self Definition',
-              'Mnemonic', 'Example Sentence']
+# For all the titles append output to result
+for j, title in enumerate(titles, start=1):
+  result.append(f'*{title}: *{lines[j]}')
 
-    # For all the titles print formatted output
-    for j, title in enumerate(titles, start=1):
-      print(f'*{title}: *{lines[i + j]}')
+# Copy the result to clipboard
+pyperclip.copy('\n'.join(result))
